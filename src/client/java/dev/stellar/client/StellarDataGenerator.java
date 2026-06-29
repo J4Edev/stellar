2accc6a786a1bc86a9ffe83cc43a7176194f1b9d
J4Edev <yae-work@proton.me>
2026-06-30 01:47:35 +0300

    Initial commit

---
+++ src/client/java/dev/stellar/client/StellarDataGenerator.java
@@ -0,0 +1,11 @@
+package dev.stellar.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class StellarDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
