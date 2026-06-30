79f6433e3dee8762d0b7cfda5394b70172dde1ee
J4Edev <yae-work@proton.me>
2026-06-30 13:00:51 +0300

    Added Commands

---
+++ src/main/java/dev/stellar/commands/ST.java
@@ -0,0 +1,30 @@
+package dev.stellar.commands;
+
+import com.mojang.brigadier.CommandDispatcher;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
+import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.chat.Component;
+
+
+import static net.fabricmc.fabric.api.client.command.v2.ClientCommands.literal;
+
+public class ST {
+
+    public static void register() {
+        ClientCommandRegistrationCallback.EVENT.register(ST::registerCommands);
+    }
+
+    private static void registerCommands(CommandDispatcher<FabricClientCommandSource> dispatcher, Object registryAccess) {
+
+        dispatcher.register(literal("st").executes(ctx -> {
+            Minecraft.getInstance().setScreenAndShow(
+                    new CustomScreen(Component.literal("Stellar UI"))
+            );
+            return 1;
+        }));
+
+        dispatcher.register(literal("stellar").redirect(dispatcher.getRoot().getChild("st")));
+        dispatcher.register(literal("ste").redirect(dispatcher.getRoot().getChild("st")));
+    }
+}
